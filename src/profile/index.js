108d8d295388ffab925e57595aeb1a60ba0f0258
Jef King <jefkin@microsoft.com>
2017-05-14 22:21:03 -0700

    less

--- src/profile/index.js
+++ src/profile/index.js
@@ -9,8 +9,6 @@ const config = {
 config.CollLink = 'dbs/' + config.DatabaseId + '/colls/' + config.CollectionId
 
 module.exports = function (context, req) {
-    let err = null;
-
     const docDbClient = new DocumentDBClient(config.Host, { masterKey: config.AuthKey });
     const query = 'SELECT * FROM c';
 
