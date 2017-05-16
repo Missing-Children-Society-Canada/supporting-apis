1802e15a48fc2c1f77da7803ba9f1d13f3936dd2
Marc GAgne <marc.gagne@hotmail.com>
2017-05-16 01:55:09 -0400

    debugging

--- src/instagram_subscriptions/index.js
+++ src/instagram_subscriptions/index.js
@@ -12,6 +12,8 @@ module.exports = function (context, req) {
             context.res.sendStatus(400);
         }
     } else {
+        //DEBUG
+        context.log(JSON.stringify(req.body, null, 4));
         // map all updates to array of user_id & media_id messages, sent to queue
         var data = req.body.map(item => JSON.stringify(
             {
