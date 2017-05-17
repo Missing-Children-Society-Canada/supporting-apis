090b6a2cbab0d50e24c031992ddc3d2cbd9560c1
Marc GAgne <marc.gagne@hotmail.com>
2017-05-17 17:59:39 -0400

    Initial profile dataset

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,53 +1,135 @@
 const DocumentDBClient = require('documentdb').DocumentClient;
 const config = {
-    DatabaseId: "reporting",
-    CollectionId: "profile",
+    CollLink: 'dbs/reporting/colls/events',
     Host: process.env.DocDb_Host,
     AuthKey: process.env.DocDb_AuthKey,
 };
 
-config.CollLink = 'dbs/' + config.DatabaseId + '/colls/' + config.CollectionId
+
+// Gets the full name from the event
+function getNameFromEvent(event) {
+    switch(event.response.platform.toLowerCase()) {
+        case 'instagram':
+            return event.response.data.full_name;
+        case 'facebook':
+            return event.response.data.first_name + ' ' + event.response.data.last_name;
+        case 'twitter':
+            return event.response.data.name;
+        default:
+            return "unknown";
+    }
+}
+
+// Gets the users profile photo from the event
+function getUserPhotoFromEvent(event) {
+    switch(event.response.platform.toLowerCase()) {
+        case 'instagram':
+            return event.response.data.profile_picture;
+        case 'facebook':
+            return event.response.data.picture.data.url;
+        case 'twitter':
+            // by removing '_normal' you get a much higher resolution image from Twitter
+            return event.response.data.profile_image_url.replace('_normal.', '.');
+        default:
+            return null;
+    }
+}
+
+// Gets the users birthday from the event
+function getUserBirthdayFromEvent(event) {
+    if (event.response.platform.toLowerCase() === 'facebook') {
+        return event.response.data.birthday;
+    }
+
+    if (event.user && event.user.facebook && event.user.facebook.birthday) {
+        return event.user.facebook.birthday;
+    }
+
+    return null;
+}
+
+// Gets the users gender from the event
+function getUserGenderFromEvent(event) {
+    if (event.user && event.user.facebook && event.user.facebook.gender) {
+        return event.user.facebook.gender;
+    }
+
+    return null;
+}
+
+// Gets the users twitter hande from the event
+function getTwitterHandleFromEvent(event) {
+    if (event.user && event.user.twitter && event.user.twitter.username) {
+        return event.user.twitter.username;
+    }
+
+    return null;
+}
+
+
+// Gets the users instagram hande from the event
+function getInstagramHandleFromEvent(event) {
+    if (event.user && event.user.instagram && event.user.instagram.username) {
+        return event.user.instagram.username;
+    }
+
+    return null;
+}
+
+// Gets the users instagram hande from the event
+function getFacebookHandleFromEvent(event) {
+    if (event.user && event.user.facebook && event.user.facebook.$id) {
+        return event.user.facebook.$id;
+    }
+
+    return null;
+}
+
+function assignIfNotNull(object, parameter, value, replace = true) {
+    if (value != null && value !== "" && replace === true) {
+        object[parameter] = value;
+    }
+}
 
 module.exports = function (context, req) {
-    let err = null;
+    let data = [];
 
     const docDbClient = new DocumentDBClient(config.Host, { masterKey: config.AuthKey });
-    const query = 'SELECT * FROM c';
+    const query = 'SELECT * FROM c WHERE c.response.type =\'profile\' ORDER BY c.triggeredOn DESC';
+    const options = {
+        enableCrossPartitionQuery: true
+    }
 
-    docDbClient.queryDocuments(config.CollLink, query).toArray(function (err, results) {
+    docDbClient.queryDocuments(config.CollLink, query, options).toArray(function (err, results) {
+        let profiles = {};
+        results.forEach(function (event) {
+            let userid = event.user.id;
+            
+            let profile = profiles[userid] || {
+                id: userid,
+                eventCount: 0,
+                mostRecentPlatform: event.response.platform,
+                triggeredOn: event.triggeredOn,
+                name: getNameFromEvent(event),
+                photo: getUserPhotoFromEvent(event),
+                birthday: null,
+                social: {}
+            };
 
-        let users = results[0];
-        //format data, return subset of results[0] data
+            profile.eventCount++;
 
-        // context.res = {
-        //     body: JSON.stringify(data)
-        // };
+            assignIfNotNull(profile, 'gender', getUserGenderFromEvent(event));
+            assignIfNotNull(profile, 'birthday', getUserBirthdayFromEvent(event));
+            assignIfNotNull(profile.social, 'twitter', getTwitterHandleFromEvent(event));
+            assignIfNotNull(profile.social, 'instagram', getInstagramHandleFromEvent(event));
+            assignIfNotNull(profile.social, 'facebook', getFacebookHandleFromEvent(event));
 
-        // context.done();
+            profiles[userid] = profile;
+        });
+
+        context.log(JSON.stringify(profiles, null, 4));
+        context.bindings.res = profiles;
+        context.done();
     });
 
-    let data = [{
-        id: '122'
-        , name: 'Fake'
-        , picture: 'http://image'
-        , datetime: 'blah'
-        , twitter: 1
-        , facebook: 1
-        , instagram: 0
-    },
-    {
-        id: '1123'
-        , name: 'Fake'
-        , picture: 'http://image'
-        , datetime: 'blah'
-        , twitter: 1
-        , facebook: 1
-        , instagram: 0
-    }];
-
-    context.res = {
-        body: JSON.stringify(data)
-    };
-
-    context.done();
 }
